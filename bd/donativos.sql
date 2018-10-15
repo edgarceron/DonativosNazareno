e5dbf79c9fc7cebded50f458f54c6fd6dbce481e
Edgar Mauricio Ceron Florez <edgar.ceron@correounivalle.edu.co>
2018-10-15 13:41:33 -0500

    Subiendo bd

---
+++ bd/donativos.sql
@@ -0,0 +1,154 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 15-10-2018 a las 20:40:18
+-- Versión del servidor: 10.1.32-MariaDB
+-- Versión de PHP: 5.6.36
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `donativos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `donaciones`
+--
+
+CREATE TABLE `donaciones` (
+  `id` int(11) NOT NULL,
+  `id_evento` int(11) NOT NULL,
+  `id_donante_donacion` int(11) NOT NULL,
+  `id_representante_donacion` int(11) DEFAULT NULL,
+  `valor_donacion` decimal(10,0) NOT NULL,
+  `validez_donacion` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `donaciones`
+--
+
+INSERT INTO `donaciones` (`id`, `id_evento`, `id_donante_donacion`, `id_representante_donacion`, `valor_donacion`, `validez_donacion`) VALUES
+(1, 1, 1, NULL, '10000', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `donantes`
+--
+
+CREATE TABLE `donantes` (
+  `id` int(11) NOT NULL,
+  `tipo_documento_donante` int(11) NOT NULL,
+  `numero_documento_donante` varchar(11) NOT NULL,
+  `nombre_donante` varchar(50) NOT NULL,
+  `apellido_donante` varchar(50) NOT NULL,
+  `direccion_donante` varchar(60) NOT NULL,
+  `correo_donante` varchar(100) NOT NULL,
+  `telefono_donante` varchar(14) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `donantes`
+--
+
+INSERT INTO `donantes` (`id`, `tipo_documento_donante`, `numero_documento_donante`, `nombre_donante`, `apellido_donante`, `direccion_donante`, `correo_donante`, `telefono_donante`) VALUES
+(1, 1, '1144166082', 'Edgar Mauricio', 'Ceron Florez', 'Calle 62B#1A9-205 Sec 4 Agrup 6 Apartamento 6E23', 'maurinin@yahoo.com', '3176483290');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `eventos`
+--
+
+CREATE TABLE `eventos` (
+  `id` int(11) NOT NULL,
+  `nombre_evento` varchar(70) NOT NULL,
+  `fecha_evento` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `eventos`
+--
+
+INSERT INTO `eventos` (`id`, `nombre_evento`, `fecha_evento`) VALUES
+(1, 'Misa dominical', '2018-10-14'),
+(2, 'Misa dominical', '2018-10-07');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `donaciones`
+--
+ALTER TABLE `donaciones`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_donante_donacion` (`id_donante_donacion`),
+  ADD KEY `id_representante_donacion` (`id_representante_donacion`),
+  ADD KEY `donaciones_ibfk_3` (`id_evento`);
+
+--
+-- Indices de la tabla `donantes`
+--
+ALTER TABLE `donantes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `tipo_documento_donante` (`tipo_documento_donante`,`numero_documento_donante`);
+
+--
+-- Indices de la tabla `eventos`
+--
+ALTER TABLE `eventos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `donaciones`
+--
+ALTER TABLE `donaciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `donantes`
+--
+ALTER TABLE `donantes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `eventos`
+--
+ALTER TABLE `eventos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `donaciones`
+--
+ALTER TABLE `donaciones`
+  ADD CONSTRAINT `donaciones_ibfk_1` FOREIGN KEY (`id_donante_donacion`) REFERENCES `donantes` (`id`),
+  ADD CONSTRAINT `donaciones_ibfk_2` FOREIGN KEY (`id_representante_donacion`) REFERENCES `donantes` (`id`),
+  ADD CONSTRAINT `donaciones_ibfk_3` FOREIGN KEY (`id_evento`) REFERENCES `eventos` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
