3d15dae6e2d0d8a71169a664f9fdc21f2fd29c98
edgarceron <edgar.ceron@correounivalle.edu.co>
2019-01-30 13:00:21 -0500

    Corrección git ignore

---
+++ protected/runtime/gii-1.1.15/ModelCode.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'template' => 'default',
+  'connectionId' => 'tienda',
+  'tablePrefix' => '',
+  'modelPath' => 'application.models',
+  'baseClass' => 'CActiveRecord',
+  'buildRelations' => '1',
+  'commentsAsLabels' => '0',
+);
